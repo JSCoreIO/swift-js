5f0e4937214b85bcdcf71e7891ce72e74e32f273
Eric Summers <eric_summers@icloud.com>
2023-09-02 17:32:32 -0400

    wip

--- Sources/JSRuntime/include/JSObject.h
+++ Sources/JSRuntime/include/JSObject.h
@@ -10,6 +10,9 @@ class JSObject;
 void _retainJSObject(JSObject* _Nonnull obj);
 void _releaseJSObject(JSObject* _Nonnull obj);
 
+// This is exposed to Swift as a struct reference type, so it can't be subclassed. I would like to allow implementation on the Swift side possibly with Macros. 
+// How should that work? Could be with a struct in Swift that gets added using generics to C++ side?
+// Can this be added as a swiftui view?
 class JSObject {
 public:
 	// Make a new object from context
